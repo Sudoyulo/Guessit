b7c401e4105995fc378542aaa78c431c42a8f2d2
Brenon Parry <brenonparry@gmail.com>
2022-02-11 17:46:16 -0800

    move INSERTS for avatar to top of file

--- server/db/seeds/01_data.sql
+++ server/db/seeds/01_data.sql
@@ -1,3 +1,6 @@
+INSERT INTO avatars (avatar_url) VALUES ('/Users/brenonparry/Desktop/avatars/user-astronaut-solid.svg');
+INSERT INTO avatars (avatar_url) VALUES ('/Users/brenonparry/Desktop/avatars/user-ninja-solid.svg');
+INSERT INTO avatars (avatar_url) VALUES ('/Users/brenonparry/Desktop/avatars/user-tie-solid.svg');
 -- insert into table. initial data like games and players
 INSERT INTO users (avatar_id, date_started, player_id) VALUES (1, '2022-01-01 00:00:00', 'x7d3k6');
 INSERT INTO users (avatar_id, date_started, player_id) VALUES (2, '2022-01-01 00:00:00', 'h6m2b5');
@@ -12,6 +15,3 @@ INSERT INTO user_game (user_id, game_id, turns_taken, completed_on, started_on) 
 INSERT INTO user_game (user_id, game_id, turns_taken, completed_on, started_on) VALUES (2, 1, 3, '2022-01-01 01:20:23', '2022-01-01 01:45:15');
 INSERT INTO user_game (user_id, game_id, turns_taken, completed_on, started_on) VALUES (3, 1, 4, '2022-01-01 01:11:55', '2022-01-01 01:30:35');
 
-INSERT INTO avatars (avatar_url) VALUES ('/Users/brenonparry/Desktop/avatars/user-astronaut-solid.svg');
-INSERT INTO avatars (avatar_url) VALUES ('/Users/brenonparry/Desktop/avatars/user-ninja-solid.svg');
-INSERT INTO avatars (avatar_url) VALUES ('/Users/brenonparry/Desktop/avatars/user-tie-solid.svg');
