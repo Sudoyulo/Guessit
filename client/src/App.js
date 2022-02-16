4e949349c7c3edb129f682297867871e32051bf2
Brenon Parry <brenonparry@gmail.com>
2022-02-16 10:12:57 -0800

    added routes and cookies

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,57 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Cookies from 'js-cookie'
 
 import GameTitle from './components/gameTitle';
 import Blank from './components/blank';
+import Landing from './components/Landing';
 
 function App() {
 
+  const Paths = () => {
+    const authenticate = () => {
+      setAuth(true)
+      Cookies.set("user", "authTrue")
+    }
+    return (
+      <Routes>
+        {!auth && (<Route path='/landing' element={<Landing authenticate={authenticate}/>} />)}
+        {auth && (<Route path='/' element={<GameTitle rightSidebar={rightSidebar} setRightSidebar={setRightSidebar} leftSidebar={leftSidebar} setLeftSidebar={setLeftSidebar} />} /> )}
+        <Route path='*' element={<Navigate to={auth? '/' : 'landing'}/>}/>
+      </Routes>
+    )
+  }
+
+  const readCookie = () => {
+    const user = Cookies.get('user')
+    if (user) {
+      setAuth(true)
+    }
+  }
+
+  useEffect(()=>{
+    readCookie()
+  }, [])
+
+  const [auth, setAuth] = useState(false)
   const [leftSidebar, setLeftSidebar] = useState(<Blank />)
   const [rightSidebar, setRightSidebar] = useState(<Blank />)
 
   return (
     <Fragment>
-
-      <div className='left-sidebar' >
-        {leftSidebar}
-      </div>
-      <div className='main-view' >
-        <GameTitle rightSidebar={rightSidebar} setRightSidebar={setRightSidebar} leftSidebar={leftSidebar} setLeftSidebar={setLeftSidebar} />
-
-      </div>
-      <div className='right-sidebar'>
-        {rightSidebar}
-      </div>
+      
+        <Router>
+          <div className='left-sidebar' >
+            {leftSidebar}
+          </div>
+          <div className='main-view' >
+            <Paths />
+          </div>
+          <div className='right-sidebar'>
+            {rightSidebar}
+          </div>
+        </Router>
 
     </Fragment>
   );
