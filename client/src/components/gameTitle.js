8929b5843664e0a048a15a7dfb70f404269223ff
Kevin Tan Yulo <yulokevin@gmail.com>
2022-02-17 19:53:15 -0500

    turns taken shows up

--- client/src/components/gameTitle.js
+++ client/src/components/gameTitle.js
@@ -149,7 +149,7 @@ const GameTitle = (props) => {
     if (leftSidebar.type.name === "Followers") {
       setLeftSidebar(<Blank />)
     } else {
-      setLeftSidebar(<Followers user_id={user[0].user_id} userinfo={user} userAvatar={user[0].avatar_url} userInitials={user[0].initials} />)
+      setLeftSidebar(<Followers user_id={user[0].user_id} userinfo={user} userAvatar={user[0].avatar_url} userInitials={user[0].initials} gameid={game.id} />)
     }
   }
 
